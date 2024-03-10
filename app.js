62f04404fcb2ca4d362f885aab50a9c0f0bd53e1
fadesyphe <pivkin.jura2017@gmail.com>
2024-03-11 03:31:26 +0600

    Адаптация, добавление изменения цвета в header, подправление карусели участников и создание карусели этапов на мобильных устройствах

--- app.js
+++ app.js
@@ -3,39 +3,125 @@ const memberArrowRight = document.querySelector("#MEMBER-ARROW-RIGHT");
 const memberCarousel = document.querySelector(".members-carousel");
 const memberLine = document.querySelector(".members-line");
 const memberItem = document.querySelector('.members-item');
-const countSlider = document.querySelector('.member-arrow-wrapper p span');
-let minCount = Math.round(memberCarousel.clientWidth / (memberItem.clientWidth + 20));
-let maxCount = memberLine.childElementCount
-let widthMember = memberItem.clientWidth;
-let offsetX = 0;
-let count = 0;
-document.addEventListener("DOMContentLoaded", () => {
-	countSlider.innerHTML = count = Math.round(memberCarousel.clientWidth / (memberItem.clientWidth + 20))
-})
-document.addEventListener("resize", () => {
-	widthMember = memberItem.offsetWidth;
-})
-console.log(maxCount)
-memberArrowLeft.addEventListener("click", () => {
-	offsetX += widthMember + 20
+const countSliderMembers = document.querySelector('.arrow-wrapper--member p span');
 
-	if (offsetX > (widthMember * minCount + 20 * minCount)) {
-		offsetX = 0
-		count = minCount
+let minCountMember = Math.round(memberCarousel.clientWidth / (memberItem.clientWidth + 20));
+let maxCountMember = memberLine.childElementCount
+let widthMember = memberItem.clientWidth;
+let offsetMember = 0;
+let countMember = 0;
+
+memberArrowLeft.addEventListener("click", () => {
+	offsetMember += widthMember + 20
+	if (countMember == maxCountMember) {
+		offsetMember = 0
+		countMember = minCountMember
 	} else {
-		count++
+		countMember++
 	}
-	countSlider.innerHTML = count
-	memberLine.style.left = '-' + offsetX + 'px'
+	countSliderMembers.innerHTML = countMember
+	memberLine.style.left = '-' + offsetMember + 'px'
 })
+
 memberArrowRight.addEventListener("click", () => {
-	offsetX -= widthMember + 20;
-	if (offsetX < 0) {
-		offsetX = widthMember * 3 + 60
-		count = maxCount
+	offsetMember -= widthMember + 20;
+	if (countMember == minCountMember) {
+		offsetMember = widthMember * (maxCountMember - minCountMember) + (maxCountMember - minCountMember) * 20
+		countMember = maxCountMember
 	} else {
-		count--
+		countMember--
 	}
-	countSlider.innerHTML = count
-	memberLine.style.left = '-' + offsetX + 'px'
+	countSliderMembers.innerHTML = countMember
+	memberLine.style.left = '-' + offsetMember + 'px'
+})
+
+// ANCHORS
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+		const blockID = anchor.getAttribute('href').substr(1)
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
+}
+
+// STAGE SLIDER
+const stageArrowLeft = document.querySelector("#STAGE-ARROW-LEFT");
+const stageArrowRight = document.querySelector("#STAGE-ARROW-RIGHT");
+const stageCarousel = document.querySelector(".stage-carousel");
+const stageLine = document.querySelector(".stage-line");
+const stageItem = document.querySelector(".stage-line .stage-item");
+const dotsStage = document.querySelectorAll(".dots-wrapper span")
+
+let stageItemWidth = stageItem.clientWidth;
+let maxCountStage = stageLine.childElementCount;
+let countStage = 1;
+let offsetStage = 0;
+
+stageArrowLeft.addEventListener("click", () => {
+	if (stageArrowLeft.classList.contains('disabled')) {
+		return
+	}
+	countStage++
+	toggleClassDots()
+	checkArrowDisabled()
+	offsetStage += stageItemWidth + 20;
+	stageLine.style.left = '-' + offsetStage + 'px'
+})
+
+stageArrowRight.addEventListener("click", () => {
+	if (stageArrowRight.classList.contains('disabled')) {
+		return
+	}
+	countStage--
+	toggleClassDots()
+	checkArrowDisabled()
+	offsetStage -= stageItemWidth + 20;
+	stageLine.style.left = '-' + offsetStage + 'px'
+})
+
+for (let dot of dotsStage) {
+	dot.addEventListener("click", (e) => {
+		countStage = dot.dataset.id
+		toggleClassDots()
+		checkArrowDisabled()
+		offsetStage = stageItemWidth * (dot.dataset.id - 1) + 20 * (dot.dataset.id - 1)
+		stageLine.style.left = '-' + offsetStage + 'px'
+	})
+}
+
+function checkArrowDisabled() {
+	if (countStage == maxCountStage) {
+		stageArrowLeft.classList.add('disabled');
+		stageArrowRight.classList.remove("disabled")
+	} else if (countStage == 1) {
+		stageArrowLeft.classList.remove("disabled")
+		stageArrowRight.classList.add("disabled")
+	}
+	else {
+		stageArrowLeft.classList.remove("disabled")
+		stageArrowRight.classList.remove("disabled");
+	}
+}
+
+function toggleClassDots() {
+	dotsStage.forEach(element => {
+		element.classList.remove("active")
+		if (element.dataset.id == countStage) {
+			element.classList.add("active")
+		}
+	});
+}
+document.addEventListener("DOMContentLoaded", () => {
+	countSliderMembers.innerHTML = countMember = Math.round(memberCarousel.clientWidth / (memberItem.clientWidth + 20))
+})
+
+window.addEventListener("resize", () => {
+	widthMember = memberItem.clientWidth;
+	stageItemWidth = stageItem.clientWidth
+	countSliderMembers.innerHTML = countMember = Math.round(memberCarousel.clientWidth / (memberItem.clientWidth + 20))
+	minCount = Math.round(memberCarousel.clientWidth / (memberItem.clientWidth + 20));
 })
