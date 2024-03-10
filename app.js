80cc703f2dc18f84d40ead4f6a37abfd1c0852bb
fadesyphe <pivkin.jura2017@gmail.com>
2024-03-10 21:13:26 +0600

    сверстана desktop версия

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 const memberArrowLeft = document.querySelector("#MEMBER-ARROW-LEFT");
 const memberArrowRight = document.querySelector("#MEMBER-ARROW-RIGHT");
 const memberCarousel = document.querySelector(".members-carousel");
+const memberLine = document.querySelector(".members-line");
+const memberItem = document.querySelector('.members-item');
+const countSlider = document.querySelector('.member-arrow-wrapper p span');
+let minCount = Math.round(memberCarousel.clientWidth / (memberItem.clientWidth + 20));
+let maxCount = memberLine.childElementCount
+let widthMember = memberItem.clientWidth;
+let offsetX = 0;
+let count = 0;
+document.addEventListener("DOMContentLoaded", () => {
+	countSlider.innerHTML = count = Math.round(memberCarousel.clientWidth / (memberItem.clientWidth + 20))
+})
+document.addEventListener("resize", () => {
+	widthMember = memberItem.offsetWidth;
+})
+console.log(maxCount)
 memberArrowLeft.addEventListener("click", () => {
-	memberCarousel.style.transform = 'translateX(-500px)';
+	offsetX += widthMember + 20
+
+	if (offsetX > (widthMember * minCount + 20 * minCount)) {
+		offsetX = 0
+		count = minCount
+	} else {
+		count++
+	}
+	countSlider.innerHTML = count
+	memberLine.style.left = '-' + offsetX + 'px'
+})
+memberArrowRight.addEventListener("click", () => {
+	offsetX -= widthMember + 20;
+	if (offsetX < 0) {
+		offsetX = widthMember * 3 + 60
+		count = maxCount
+	} else {
+		count--
+	}
+	countSlider.innerHTML = count
+	memberLine.style.left = '-' + offsetX + 'px'
 })
